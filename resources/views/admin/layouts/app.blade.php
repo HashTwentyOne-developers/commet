0839597bd8abcb42df7f777796fc64d7ea13c57d
HashTwentyOne <service.hash21@gmail.com>
2021-04-05 09:36:22 +0600

    Admin Dashboard Slicing

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -7,22 +7,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
         <title>Commet Multipurpose Website</title>
 
-		<!-- Favicon -->
-        <link rel="shortcut icon" type="image/x-icon" href="{{ asset('admin/assets/img/favicon.png') }}">
 
-		<!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="{{ asset('admin/assets/css/bootstrap.min.css') }}">
-
-		<!-- Fontawesome CSS -->
-        <link rel="stylesheet" href="{{ asset('admin/assets/css/font-awesome.min.css') }}">
-
-		<!-- Feathericon CSS -->
-        <link rel="stylesheet" href="{{ asset('admin/assets/css/feathericon.min.css') }}">
-
-		<link rel="stylesheet" href="{{ asset('admin/assets/plugins/morris/morris.css') }}">
-
-		<!-- Main CSS -->
-        <link rel="stylesheet" href="{{ asset('admin/assets/css/style.css') }}">
+		@include('admin.layouts.partials.style')
 
 		<!--[if lt IE 9]>
 			<script src="assets/js/html5shiv.min.js"></script>
@@ -35,23 +21,7 @@
 
         @show
 
-		<!-- jQuery -->
-        <script src="{{ asset('admin/assets/js/jquery-3.2.1.min.js') }}"></script>
-
-		<!-- Bootstrap Core JS -->
-        <script src="{{ asset('admin/assets/js/popper.min.js') }}"></script>
-        <script src="{{ asset('admin/assets/js/bootstrap.min.js') }}"></script>
-
-		<!-- Slimscroll JS -->
-        <script src="{{ asset('admin/assets/plugins/slimscroll/jquery.slimscroll.min.js') }}"></script>
-
-		<script src="{{ asset('admin/assets/plugins/raphael/raphael.min.js') }}"></script>
-		<script src="{{ asset('admin/assets/plugins/morris/morris.min.js') }}"></script>
-		<script src="{{ asset('admin/assets/js/chart.morris.js') }}"></script>
-
-		<!-- Custom JS -->
-		<script  src="{{ asset('admin/assets/js/script.js') }}"></script>
-		<script  src="{{ asset('admin/assets/js/commet/custom.js') }}"></script>
+	@include('admin.layouts.partials.style')
 
     </body>
 
---
+++ resources/views/admin/layouts/menu.blade.php
@@ -0,0 +1,63 @@
+<div class="sidebar" id="sidebar">
+    <div class="sidebar-inner slimscroll">
+        <div id="sidebar-menu" class="sidebar-menu">
+            <ul>
+                <li class="menu-title">
+                    <span>Main</span>
+                </li>
+                <li class="active">
+                    <a href="index.html"><i class="fe fe-home"></i> <span>Dashboard</span></a>
+                </li>
+
+                <li class="submenu">
+                    <a href="#"><i class="fe fe-activity"></i> <span> Blog</span> <span class="menu-arrow"></span></a>
+                    <ul style="display: none;">
+                        <li><a href="invoice-report.html">Posts</a></li>
+                        <li><a href="invoice-report.html">Category</a></li>
+                        <li><a href="invoice-report.html">Tag</a></li>
+                    </ul>
+                </li>
+                <li class="submenu">
+                    <a href="#"><i class="fe fe-document"></i> <span> Product</span> <span class="menu-arrow"></span></a>
+                    <ul style="display: none;">
+                        <li><a href="invoice-report.html">Product</a></li>
+                        <li><a href="invoice-report.html">Category</a></li>
+                        <li><a href="invoice-report.html">Tag</a></li>
+                        <li><a href="invoice-report.html">Brand</a></li>
+                    </ul>
+                </li>
+
+                <li class="submenu">
+                    <a href="#"><i class="fe fe-users"></i> <span> Users </span> <span class="menu-arrow"></span></a>
+                    <ul style="display: none;">
+                        <li><a href="login.html"> user </a></li>
+                        <li><a href="register.html"> permission </a></li>
+                        <li><a href="forgot-password.html"> Role </a></li>
+                        <li><a href="lock-screen.html"> Lock Screen </a></li>
+                    </ul>
+                </li>
+
+                <li class="submenu">
+                    <a href="#"><i class="fe fe-document"></i> <span> Pages </span> <span class="menu-arrow"></span></a>
+                    <ul style="display: none;">
+                        <li><a href="login.html"> user </a></li>
+                        <li><a href="register.html"> permission </a></li>
+                        <li><a href="forgot-password.html"> Role </a></li>
+                        <li><a href="lock-screen.html"> Lock Screen </a></li>
+                    </ul>
+                </li>
+                <li class="submenu">
+                    <a href="#"><i class="fe fe-vector"></i> <span> Settings </span> <span class="menu-arrow"></span></a>
+                    <ul style="display: none;">
+                        <li><a href="login.html"> slider </a></li>
+                        <li><a href="register.html"> Contact US </a></li>
+                        <li><a href="register.html">Social Icons</a></li>
+
+                    </ul>
+                </li>
+
+
+            </ul>
+        </div>
+    </div>
+</div>
